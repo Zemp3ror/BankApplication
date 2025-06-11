54f303c9084b1e8fc343ef8b5d6f73fdfb65fddf
Stepan <Stepan@DESKTOP-NRBG1MU>
2025-06-11 07:17:54 +0300

    Добавьте файлы проекта.

---
+++ BankLibrary/AccountEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+    public class AccountEventArgs
+    {
+        public string Message { get; } // Сообщение
+        public decimal Sum { get; } // Сумма на которую изменился счёт
+        public AccountEventArgs(string _message, decimal _sum)
+        {
+            this.Message = _message;
+            this.Sum = _sum;
+        }
+    }
+}
---
+++ BankLibrary/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public interface IAccount
+    {
+        
+        void Put(decimal sum); //Положить деньги на счёт
+        decimal Withdraw(decimal sum); //Взять со счёта
+    }
+}
